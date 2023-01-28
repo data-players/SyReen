7be750c3d6a25483f6c9b6adcf99fbf2400eb7d3
Vincentfarcy <v.farcy72@gmail.com>
2023-01-28 18:18:43 +0100

    init with mutual-aid.app

---
+++ src/config/dataServers.js
@@ -0,0 +1,21 @@
+const dataServers = {
+  pod: {
+    pod: true,
+    default: true,
+    authServer: true,
+    baseUrl: null, // Calculated from the token
+    sparqlEndpoint: null,
+    containers: {
+      pod: {
+        'mp:Offer': ['/offers'],
+        'mp:Request': ['/requests'],
+        'pair:Project': ['/projects'],
+        'vcard:Location': ['/locations'],
+        'vcard:Individual': ['/profiles'],
+      },
+    },
+    uploadsContainer: '/files',
+  },
+};
+
+export default dataServers;
