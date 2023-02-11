683b00f84feb962180664fd36ffc6acd9cb607cc
Vincentfarcy <v.farcy72@gmail.com>
2023-02-11 14:11:42 +0100

    locationInformation

--- src/commons/fields/LocationField.js
+++ src/commons/fields/LocationField.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, makeStyles } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
 import { MapField } from '@semapps/geo-components';
-import { ReferenceField } from '@semapps/field-components';
-import { TextField, useRecordContext } from 'react-admin';
+import { ReferenceField } from '@semapps/field-components'
+import MarkdownField from '../../commons/fields/MarkdownField';
+import { useRecordContext } from 'react-admin';
+
+const useStyles = makeStyles((theme) => ({
+  markdownFieldContainer: {
+    '& strong': {
+      float: 'left'
+    },
+    '& p': {
+      display: 'inline',
+      fontSize: 14
+    }
+  },
+}));
 
 const LocationField = ({ label, source, ...rest }) => {
-  const record = useRecordContext(rest);
+  const classes = useStyles();
+  const recordContext = useRecordContext(rest);
   return (
     <>
-      <ReferenceField reference="Location" record={record} source={source} link={false}>
+      <ReferenceField reference="Location" record={recordContext} source={source} link={false}>
         <MapField
-          address={(record) => (
+          address={(locationRecord) => (
             <>
-              {record?.['vcard:given-name'] + ', ' + record?.['vcard:hasAddress']?.['vcard:given-name']}
-              {record?.['vcard:note'] && (
+              {locationRecord?.['vcard:given-name'] + ', ' + locationRecord?.['vcard:hasAddress']?.['vcard:given-name']}
+              {locationRecord?.['vcard:note'] && (
                 <Box mb={2} mt={2}>
                   <Alert severity="info">
-                    <strong>Note</strong>: {record?.['vcard:note']}
+                    <strong>Note</strong>:&nbsp;{locationRecord?.['vcard:note']}
+                  </Alert>
+                </Box>
+              )}
+              {recordContext?.['syreen:locationInformation'] && (
+                <Box mb={2} mt={2} className={classes.markdownFieldContainer}>
+                  <Alert severity="info">
+                    <strong>Note</strong>:&nbsp; 
+                    <MarkdownField
+                      record={recordContext}
+                      source="syreen:locationInformation"
+                      addLabel={false}/>
                   </Alert>
                 </Box>
               )}
@@ -28,7 +53,6 @@ const LocationField = ({ label, source, ...rest }) => {
           height={250}
         />
       </ReferenceField>
-      <TextField variant="body1" record={record} source="syreen:locationInformation" />
     </>
   );
 };
