da3540466c4e28e31dcf0829ceedfbfee6d04fef
Vincentfarcy <v.farcy72@gmail.com>
2023-02-19 17:09:16 +0100

    add project concepts

--- src/resources/Project/concepts.js
+++ src/resources/Project/concepts.js
@@ -1,7 +1,9 @@
 export const concepts = {
   projectTypes: [
-    { id: 'fin-de-chantier',  name: 'Fin de Chantier' },
-    { id: 'destockage',       name: 'Déstockage' },
-    { id: 'demenagement',     name: 'Déménagement' }
+    { id: 'demenagement',    name: 'Déménagement' },
+    { id: 'demolition',      name: 'Démolition' },
+    { id: 'destockage',      name: 'Déstockage' },
+    { id: 'fin-de-chantier', name: 'Fin de Chantier' },
+    { id: 'rehabilitation',  name: 'Réhabilitation' },
   ]
 }
