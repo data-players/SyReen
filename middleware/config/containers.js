8e498c7bbf258c6acc50b98aa91dab79dea0811d
srosset81 <srosset81@gmail.com>
2023-06-07 11:14:08 +0200

    Add default read permissions to all containers

--- middleware/config/containers.js
+++ middleware/config/containers.js
@@ -1,5 +1,7 @@
 const { ACTOR_TYPES } = require("@semapps/activitypub");
 
+const defaultReadPermissions = { default: { read: true } };
+
 module.exports = [
   {
     path: '/',
@@ -8,42 +10,50 @@ module.exports = [
   {
     path: '/offers',
     acceptedTypes: ['syreen:Offer'],
+    permissions: defaultReadPermissions,
     readOnly: true
   },
   {
     path: '/projects',
     acceptedTypes: ['syreen:Project'],
+    permissions: defaultReadPermissions,
     readOnly: true
   },
   {
     path: '/actors',
     acceptedTypes: [ACTOR_TYPES.APPLICATION],
     dereference: ['sec:publicKey', 'as:endpoints'],
+    permissions: defaultReadPermissions,
     readOnly: true
   },
   {
     path: '/categories',
     acceptedTypes: ['syreen:Category'],
+    permissions: defaultReadPermissions,
     readOnly: true
   },
   {
     path: '/batiprix',
     acceptedTypes: ['syreen:BatiprixCategory'],
+    permissions: defaultReadPermissions,
     readOnly: true
   },
   {
     path: '/units',
     acceptedTypes: ['syreen:Unit'],
+    permissions: defaultReadPermissions,
     readOnly: true
   },
   {
     path: '/stages',
     acceptedTypes: ['syreen:Stage'],
+    permissions: defaultReadPermissions,
     readOnly: true
   },
   {
     path: '/types',
     acceptedTypes: ['syreen:ProjectType'],
+    permissions: defaultReadPermissions,
     readOnly: true
   }
 ];
