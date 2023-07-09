eb541bf10f60604c9a2531342418ab642bda5a32
srosset81 <srosset81@gmail.com>
2023-07-09 23:52:29 +0200

    Add more actor types

--- frontend/src/commons/inputs/ActorTypeSelectInput.js
+++ frontend/src/commons/inputs/ActorTypeSelectInput.js
@@ -2,13 +2,14 @@ import React from "react";
 import { SelectInput } from "react-admin";
 
 const actorTypes = [
-  { name: 'Un artisan' },
-  { name: 'Un bureau d’étude, un architecte un AMOE' },
+  { name: 'Un.e artisan.e' },
+  { name: 'Un bureau d’étude, un.e architecte, un.e AMO' },
   { name: 'Une collectivité' },
   { name: 'Un distributeur' },
+  { name: 'Une recyclerie' },
   { name: 'Un acteur du déchet' },
+  { name: 'Autre' }
 ];
-
 
 const ActorTypeSelectInput = props => (
   <SelectInput {...props} translateChoice={false} optionValue="name" choices={actorTypes} />
